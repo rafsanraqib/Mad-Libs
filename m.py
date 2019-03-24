1dfab1a62497becf0193dd6c40a992711402b7d3
Rafsan Raqib <raqibahmed035@gmail.com>
2019-03-24 16:30:33 -0500

    Initial add to repository

---
+++ m.py
@@ -0,0 +1,41 @@
+print("Noun: ")
+noun = input()
+
+print("Geographical Location: ")
+g_location = input()
+
+print("Number: ")
+number = input()
+
+print("Adverb: ")
+adverb = input()
+
+print("Silly Word: ")
+silly_word = input()
+
+print("Noun: ")
+noun1 = input()
+
+print("Silly Word: ")
+silly_word1 = input()
+
+print("Verb (Past Tense): ")
+verb_past_tense = input()
+
+print("Silly Word: ")
+silly_word2 = input()
+
+print("Exclamation: ")
+exclamation = input()
+
+print("Silly Word: ")
+silly_word3 = input()
+
+print("Silly Word: ")
+silly_word4 = input()
+
+print("Silly Word: ")
+silly_word5 = input()
+
+story = "Major Zarnak of the intergalactic space patrol deactivated his hyper " + noun + " overdrive and landed his spaceship on the planet " + g_location + " leaving the decompression chamber, he saw a " + number + " -armed monster approaching " + adverb + " he shouted, " + silly_word + " the glactic word for peace, but the monster whipped out a disintegrator " + noun1 + " and tried to " + silly_word1 + " him Major Zarnak ducked and " + verb_past_tense + " the monster with his subsonic " + silly_word2 + " " + exclamation + " said the monster, cluthing his " + silly_word3 + silly_word4 + " replied Major Zarnak cleverly, and, getting into his spaceship, he zipped back to his headquarters on the planet " + silly_word5
+print(story)
